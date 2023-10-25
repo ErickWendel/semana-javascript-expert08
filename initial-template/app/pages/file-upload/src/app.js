d55fc10f2eeafd103a45cc24c86835b5095b0103
oliveirany <flavioranieli.dev@gmail.com>
2023-10-25 18:45:36 -0300

    first commit

--- initial-template/app/pages/file-upload/src/app.js
+++ initial-template/app/pages/file-upload/src/app.js
@@ -1,48 +1,49 @@
 import Clock from './deps/clock.js';
-
-const fileUpload = document.getElementById('fileUpload')
-const btnUploadVideo = document.getElementById('btnUploadVideos')
-const fileSize = document.getElementById('fileSize')
-const fileInfo = document.getElementById('fileInfo')
-const txtfileName = document.getElementById('fileName')
-const fileUploadWrapper = document.getElementById('fileUploadWrapper')
-const elapsed = document.getElementById('elapsed')
-
-
-fileUpload.addEventListener('change', onChange)
-btnUploadVideo.addEventListener('click', () => {
-    // trigger file input
-    fileUpload.click()
-})
-let took = ''
-
-function parseBytesIntoMBAndGB(bytes) {
-    const mb = bytes / (1024 * 1024)
-    // if mb is greater than 1024, then convert to GB
-    if (mb > 1024) {
-        // rount to 2 decimal places
-        return `${Math.round(mb / 1024)}GB`
-    }
-    return `${Math.round(mb)}MB`
-}
+import View from './view.js';
+const view = new View();
 const clock = new Clock()
 
-function onChange(e) {
-    const file = e.target.files[0]
-    const { name, size } = file
-    txtfileName.innerText = name
-    fileSize.innerText = parseBytesIntoMBAndGB(size)
-
-    fileInfo.classList.remove('hide')
-    fileUploadWrapper.classList.add('hide')
-
+let took = ''
+view.configureOnFileChange(file => {
     clock.start((time) => {
         took = time;
-        elapsed.innerText = `Process started ${time}`
+        view.updateElapsedTime(`Process started ${time}`)
     })
 
     setTimeout(() => {
         clock.stop()
-        elapsed.innerText = `Process took ${took.replace('ago', '')}`
+        view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
     }, 5000)
+})
+
+async function fakeFetch() {
+    const filePath = '/videos/frag_bunny.mp4'
+    const response = await fetch(filePath)
+    
+    // traz o tamanho do arquivo em bytes
+    // const response = await fetch(filePath, {
+    //     method: "HEAD"
+    // })
+    // response.headers.get('content-length')
+    // debugger
+    
+    const file = new File([await response.blob()], filePath, {
+        type: 'video/mp4',
+        lastModified: Date.now()
+    })
+    const event = new Event('change')
+    Reflect.defineProperty(
+        event,
+        'target',
+        { value: { files: [file] } }
+    )
+    
+    document.getElementById('fileUpload').dispatchEvent(event)
 }
+
+fakeFetch()
+
+
+
+
+
---
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,1 @@
+// parei aqui - minuto 33:00 da aula 2
